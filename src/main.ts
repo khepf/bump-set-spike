578d6ec695704526d3835b76e04510c462325984
Jeremy Kepf <jeremy1975@gmail.com>
2020-01-15 13:22:16 -0500

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,32 @@
 import Vue from "vue";
+import firebase from "firebase";
+import Vuelidate from "vuelidate";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import vuetify from "./plugins/vuetify";
+import vuetify from "@/plugins/vuetify";
+
+Vue.use(Vuelidate);
 
 Vue.config.productionTip = false;
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
+};
+console.log("!!!", firebaseConfig);
+console.log("***", process.env);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 new Vue({
   router,
