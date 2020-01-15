f36a6ea8542cabb2bce09ef2d6347beb2d3ece59
Jeremy Kepf <jeremy1975@gmail.com>
2020-01-15 16:36:00 -0500

    add notfound page

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container class="notfound" fill-height>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8">
+        <h1>Page Not Found</h1>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8">
+        <p>
+          Ooops, we couldn't find what you are looking for. Why don't you
+          <router-link :to="{ name: 'Home' }">
+            go home instead?
+          </router-link>
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "landing"
+};
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,29 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Landing from "../pages/PageLanding.vue";
-import Home from "../pages/PageHome.vue";
+import NotFound from "../pages/PageNotFound.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "landing",
+    name: "Landing",
     component: Landing
   },
   {
     path: "/home",
-    name: "home",
+    name: "Home",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "home" */ "@/pages/PageHome.vue")
+  },
+  {
+    path: "*",
+    name: "NotFound",
+    component: NotFound
   }
 ];
 
