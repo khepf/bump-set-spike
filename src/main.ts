3f835b1f17f387e9776d016fcf3f5f2fd0f99d0b
Jeremy Kepf <jeremy1975@gmail.com>
2020-01-19 16:40:09 -0500

    register page convert to vuetify

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import router from "./router";
 import store from "./store";
 import vuetify from "@/plugins/vuetify";
 import AppDate from "@/components/AppDate.vue";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 Vue.use(Vuelidate);
 Vue.component("AppDate", AppDate);
